72bc0c1a71a501c7cfdbe1932d56cfbc26d67d9f
Michael Yatco <mjyatco@gmail.com>
2017-02-13 21:31:02 -0500

    Added Environmental Variable (.env)

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,42 @@
 class StudentsController < ApplicationController
+  
+  def index
+    @students = Unirest.get("#{ENV['API_URL']}students/").body
+    render "index.html.erb"
+  end
+
   def show
-    @student = Unirest.get("http://localhost:3000/api/v2/students/#{params[:id]}").body
+    @student = Unirest.get("#{ENV['API_URL']}students/#{params[:id]}").body
     render "show.html.erb"
   end
+
+  def new
+    render "new.html.erb" 
+  end
+
+  def create
+    @student = Unirest.post("#{ENV['API_URL']}students/", 
+      headers:{ "Accept" => "application/json"},
+      parameters:{ first_name: params[:first_name], last_name: params[:last_name], birthdate: params[:birthdate], university: params[:university], pokemon: params[:pokemon], email: params[:email]}).body
+    redirect_to "/students/#{@student['id']}"
+  end
+
+  def edit
+    @student = Unirest.get("#{ENV['API_URL']}students/#{params[:id]}").body
+    render "edit.html.erb"
+  end
+
+  def update
+    @student = Unirest.patch("#{ENV['API_URL']}students/#{params[:id]}", 
+      headers:{ "Accept" => "application/json"},
+      parameters:{ first_name: params[:first_name], last_name: params[:last_name], birthdate: params[:birthdate], university: params[:university], pokemon: params[:pokemon], email: params[:email]}).body
+    redirect_to "/students/#{@student['id']}"
+  end
+
+  def destroy
+    @student = Unirest.delete("#{ENV['API_URL']}students/#{params[:id]}",
+      headers:{ "Accept" => "application/json"}).body
+    redirect_to "/students"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/students/:id", to: "students#show"
   get "/students", to: "students#index"
+  get "/students/new", to: "students#new"
+  post "/students", to: "students#create"
+  get "/students/:id", to: "students#show"
+  get "/students/:id/edit", to: "students#edit"
+  patch "/students/:id", to: "students#update"
+  delete "/students/:id", to: "students#destroy"
 end
